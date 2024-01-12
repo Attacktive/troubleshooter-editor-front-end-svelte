c521e4d362381683909a7e87c31e50f491b1933d
Attacktive <tribute2pantera@gmail.com>
2024-01-12 21:05:53 +0900

    chore: apply `.editorconfig`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	base: "/troubleshooter-editor-front-end-svelte",
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
 });
