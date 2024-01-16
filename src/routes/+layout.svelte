2c337161b1705f57a38d4c1f773a9dd4495dd759
Attacktive <tribute2pantera@gmail.com>
2024-01-16 23:08:17 +0900

    chore: configure Flowbite Svelte
    https://github.com/Attacktive/troubleshooter-editor-front-end-svelte/issues/2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import "../app.css";
+	import Header from "./Header.svelte";
+	import Footer from "./Footer.svelte";
+</script>
+
+<div class="p-3">
+	<Header/>
+</div>
+<div class="px-3 py-1">
+	<slot/>
+</div>
+<div class="px-3">
+	<Footer/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button, ButtonGroup, Fileupload, Spinner, TabItem, Tabs, Textarea } from "flowbite-svelte";
+
+	$: toShowSpinner = false;
+	$: fileIsSelected = true;
+	$: fileIsUploaded = false;
+	$: debuggingOutput = "";
+</script>
+
+{#if toShowSpinner}
+	<Spinner color="red" size="20"/>
+{/if}
+
+<div class="grid grid-cols-12 mt-2">
+	<Fileupload accept={".sav,.bak"} class="col-span-3"/>
+	<ButtonGroup class="flex justify-around col-span-6">
+		<Button disabled={!fileIsSelected}>Upload</Button>
+		<Button disabled={!fileIsSelected || !fileIsUploaded}>Save</Button>
+		<Button disabled={!fileIsSelected || !fileIsUploaded}>Quick Cheats!</Button>
+	</ButtonGroup>
+</div>
+<div class="mt-4">
+	<Tabs>
+		<TabItem title="Company" open>
+
+		</TabItem>
+		<TabItem title="Items"></TabItem>
+		<TabItem title="Rosters"></TabItem>
+		<TabItem title="Quests"></TabItem>
+	</Tabs>
+</div>
+<div class="mt-4">
+	<Textarea value={debuggingOutput} rows="6" readonly/>
+</div>
