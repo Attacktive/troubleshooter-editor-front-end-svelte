61c542413a747bdbb737baf0620c2cc2b7bad928
Attacktive <tribute2pantera@gmail.com>
2024-08-01 11:05:00 +0900

    feature: upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs["flat/recommended"],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ["**/*.svelte"],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		},
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					"argsIgnorePattern": "^_",
+					"varsIgnorePattern": "^_",
+					"caughtErrorsIgnorePattern": "^_"
+				}
+			]
+		}
+	},
+	{
+		ignores: ["build/", ".svelte-kit/", "dist/"]
+	}
+];
