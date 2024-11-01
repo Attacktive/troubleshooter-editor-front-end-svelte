af3533df9576e3d7b357263adf234ca34b0ee516
Attacktive <tribute2pantera@gmail.com>
2024-11-01 16:22:02 +0900

    fix: misconfiguration for test
    https://github.com/Attacktive/troubleshooter-editor-front-end-svelte/issues/416

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,19 @@
+import type { CompileOptions } from "svelte/compiler";
 import { defineConfig } from "vitest/config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+const compilerOptions: CompileOptions = {
+	hmr: !process.env.VITEST
+};
+
 export default defineConfig({
 	plugins: [
-		svelte({ hot: !process.env.VITEST })
+		svelte({ compilerOptions })
 	],
 	test: {
 		environment: "jsdom"
+	},
+	resolve: {
+		conditions: ["browser"]
 	}
 });
