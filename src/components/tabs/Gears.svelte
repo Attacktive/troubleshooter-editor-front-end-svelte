5189e186d267eada246307dcad5cdc01bd23dd8d
Attacktive <tribute2pantera@gmail.com>
2024-12-07 23:51:51 +0900

    fix: broken gear editing

--- src/components/tabs/Gears.svelte
+++ src/components/tabs/Gears.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import { store } from "$store/store";
-	import { Input, Label } from "flowbite-svelte";
-	import Items from "$components/tabs/Items.svelte";
+	import { Accordion, AccordionItem, Input, Label, NumberInput, Select, Textarea } from "flowbite-svelte";
 
 	const optionKeyOptions = ["Abyss", "Adventurer", "Archisage", "Ascetic", "Assassin", "Barbarian", "Bear", "Berserker", "Butcher", "Chaser", "Clown", "Collector", "Conqueror", "Crocodile", "Cyclopes", "DarkChaser", "DarkCollector", "Deathblow", "Destiny", "Elephant", "Emperor", "Ettin", "Explorer", "Extreme", "Fighter", "FlameArrow", "FlameBlade", "FlameDestroyer", "FlameFanatic", "FlameGuardian", "FlameMage", "FlameOverload", "FlameRuler", "FlameScout", "FlameSentinel", "FlameShield", "FlameSoldier", "FlameWall", "FlashArrow", "FlashBlade", "FlashDestroyer", "FlashFanatic", "FlashGuardian", "FlashMage", "FlashOverload", "FlashRuler", "FlashScout", "FlashSentinel", "FlashShield", "FlashSoldier", "FlashWall", "Fortress", "Fox", "FrostArrow", "FrostBlade", "FrostDestroyer", "FrostFanatic", "FrostGuardian", "FrostMage", "FrostOverload", "FrostRuler", "FrostScout", "FrostSentinel", "FrostShield", "FrostSoldier", "FrostWall", "Giant", "Gorilla", "Guide", "Gunman", "Hero", "HighSpeed", "Hippopotamus", "Hunter", "Infallibility", "IronWall", "King", "Knight", "Lion", "Looter", "Luck", "Lupin", "MartialArtist", "Messiah", "MeteorGuardian", "MeteorScout", "MeteorSentinel", "MeteorShield", "MeteorWall", "Miracle", "Mirage", "MistArrow", "MistBlade", "MistFanatic", "MistGuardian", "MistMage", "MistRuler", "MistScout", "MistSentinel", "MistShield", "MistSoldier", "MistWall", "Monkey", "MoonGuardian", "MoonScout", "MoonSentinel", "MoonShield", "MoonWall", "Overload", "Pioneer", "Prophet", "Scout", "Sentinel", "Sequent", "Settler", "Shadow", "Sharpshooter", "Sniper", "Soul", "SoulChaser", "SoulCollector", "SoulGuardian", "SoulGuide", "Spirit", "SpiritGuardian", "StarGuardian", "StarScout", "StarSentinel", "StarShield", "StarWall", "StormArrow", "StormBlade", "StormDestroyer", "StormFanatic", "StormGuardian", "StormMage", "StormScout", "StormSentinel", "StormShield", "StormSoldier", "StormWall", "StrangeChance", "Strategist", "Superman", "Swordman", "Swordmaster", "Thief", "Traveler", "Turtle", "TwilightChaser", "TwilightCollector", "VerdureArrow", "VerdureBlade", "VerdureFanatic", "VerdureGuardian", "VerdureMage", "VerdureRuler", "VerdureScout", "VerdureSentinel", "VerdureShield", "VerdureSoldier", "VerdureWall", "Victory", "Warrior", "Wolf", "Zenith"];
 	const propertyOptions = ["Accuracy", "Armor", "AttackPower", "Block", "BluntResistance", "CriticalStrikeChance", "CriticalStrikeDeal", "Dodge", "EarthResistance", "ESPPower", "FireResistance", "IceResistance", "LightningResistance", "MaxAddSP", "MaxHP", "MaxVigor", "MoveDist", "OverchargeDuration", "PiercingResistance", "RegenVigor", "Resistance", "SightRange", "SlashingResistance", "Speed", "WaterResistance", "WindResistance"];
-
-	const gears = $state($store.gears);
-	//let gearId: number;
 </script>
 
 <datalist id="option-keys-datalist">
@@ -21,24 +17,56 @@
 	{/each}
 </datalist>
 
-<Items items={gears} >
-	{#snippet children({ index })}
-		{@const gearId = gears[index].id}
-		<div class="my-1">
-			<Label for={`option-ratio-${gearId}`}>Option Ratio</Label>
-			<Input id={`option-ratio-${gearId}`} bind:value={gears[index].properties["Option/Ratio"]}/>
-		</div>0
-		<div class="my-1">
-			<Label for={`option-key-${gearId}`}>Option Key</Label>
-			<Input id={`option-key-${gearId}`} list="option-keys-datalist" bind:value={gears[index].properties["Option/OptionKey"]}/>
-		</div>
+<div class="my-1">
+	<Accordion>
+		{#each $store.gears as gear (`gear-${gear.id}`)}
+			<AccordionItem>
+				{#snippet header()}
+					<span>#{gear.id} {gear.type}</span>
+				{/snippet}
+				<div class="my-1">
+					<Label for={`type-${gear.id}`}>Type</Label>
+					<Input id={`type-${gear.id}`} bind:value={gear.type}/>
+				</div>
+				<div class="my-1">
+					<Label for={`count-${gear.id}`}>Count</Label>
+					<NumberInput id={`count-${gear.id}`} bind:value={gear.count}/>
+				</div>
+				<div class="my-1">
+					<Label for={`status-${gear.id}`}>Status</Label>
+					<Select id={`status-${gear.id}`} bind:value={gear.status}>
+						<option value="inventory">Inventory</option>
+						<option value="warehouse">Warehouse</option>
+						<option value="equipped">Equipped</option>
+						<option value="stasis">Stasis</option>
+						<option value="mailbox">Mailbox</option>
+					</Select>
+				</div>
 
-		{#each [1, 2, 3, 4, 5] as ordinal (`item-${gearId}-property-${ordinal}`)}
-			<div class="my-1">
-				<Label for={`property-${gearId}-${ordinal}`}>Property {ordinal}</Label>
-				<Input id={`property-${gearId}-${ordinal}`} list="options-datalist" bind:value={gears[index].properties[`Option/Type${ordinal}`]}/>
-				<Input bind:value={gears[index].properties[`Option/Value${ordinal}`]}/>
-			</div>
+				<div class="my-1">
+					<Label for={`option-ratio-${gear.id}`}>Option Ratio</Label>
+					<Input id={`option-ratio-${gear.id}`} bind:value={gear.properties["Option/Ratio"]}/>
+				</div>0
+				<div class="my-1">
+					<Label for={`option-key-${gear.id}`}>Option Key</Label>
+					<Input id={`option-key-${gear.id}`} list="option-keys-datalist" bind:value={gear.properties["Option/OptionKey"]}/>
+				</div>
+
+				{#each [1, 2, 3, 4, 5] as ordinal (`item-${gear.id}-property-${ordinal}`)}
+					<div class="my-1">
+						<Label for={`property-${gear.id}-${ordinal}`}>Property {ordinal}</Label>
+						<Input id={`property-${gear.id}-${ordinal}`} list="options-datalist" bind:value={gear.properties[`Option/Type${ordinal}`]}/>
+						<Input bind:value={gear.properties[`Option/Value${ordinal}`]}/>
+					</div>
+				{/each}
+
+				<Label class="mt-4">raw data</Label>
+				<Accordion>
+					<AccordionItem>
+						<Textarea value={JSON.stringify(gear.properties, null, 2)} rows={6} readonly/>
+					</AccordionItem>
+				</Accordion>
+			</AccordionItem>
 		{/each}
-	{/snippet}
-</Items>
+	</Accordion>
+</div>
